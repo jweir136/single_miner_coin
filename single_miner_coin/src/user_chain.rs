bc6abf2716f87e325940f2072eec1444fb727e24
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-05 20:39:12 -0500

    add hash() method

--- single_miner_coin/src/user_chain.rs
+++ single_miner_coin/src/user_chain.rs
@@ -4,3 +4,16 @@ use block_cryptography_rust::hashing::sha256_hash;
 type Username = String;
 type Hash = Digest;
 type PublicKey = &[u8];
+
+struct UserBlock {
+    username: Username,
+    key: PublicKey,
+    last: Hash,
+    proof: Hash
+}
+
+impl UserBlock {
+    pub fn hash(&self) -> Hash {
+        sha256_hash(format!("{}{}", self.username, self.key))
+    }
+}
