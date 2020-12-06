cfe7e0deddd4bc2441225235a3d3baf06cfdc099
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-05 20:55:15 -0500

    fix public key type, change hashing method, and add hash_test()

--- single_miner_coin/src/user_chain.rs
+++ single_miner_coin/src/user_chain.rs
@@ -3,7 +3,7 @@ use block_cryptography_rust::hashing::sha256_hash;
 
 type Username = String;
 type Hash = Digest;
-type PublicKey = &[u8];
+type PublicKey = [u8; 32];
 
 struct UserBlock {
     username: Username,
@@ -13,7 +13,25 @@ struct UserBlock {
 }
 
 impl UserBlock {
+    
     pub fn hash(&self) -> Hash {
-        sha256_hash(format!("{}{}", self.username, self.key))
+        sha256_hash(format!("{}{:?}", self.username, self.key).as_bytes())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn hash_test() {
+        let block = UserBlock {
+            username: "test".to_string(),
+            key: [0 as u8; 32],
+            last: sha256_hash(format!("test").as_bytes()),
+            proof: sha256_hash(format!("test").as_bytes())
+        };
+
+        block.hash();
     }
 }
