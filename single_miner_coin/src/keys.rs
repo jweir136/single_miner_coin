4576fccb3a2a4d502a3995fae2e45554660675d8
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-07 15:41:38 -0500

    add get_user() methods and add test

--- single_miner_coin/src/keys.rs
+++ single_miner_coin/src/keys.rs
@@ -9,3 +9,20 @@ fn add_user(key_map: &mut HashMap<Username, PublicKey>, username: Username, key:
         None   => { None }
     }
 }
+
+fn get_user<'a>(key_map: &'a HashMap<Username, PublicKey>, username: &'a Username) -> Option<&'a PublicKey> {
+    key_map.get(username)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let mut map = std::collections::HashMap::<Username, PublicKey>::new();
+
+        add_user(&mut map, "Jake".to_string(), [0 as u8; 32]);
+        get_user(&map, &"Jake".to_string());
+    }
+}
