1a82ddd677380f33d7c6e80e8169c243f5c94401
Jacob Weir <jacobweir@Jacobs-MacBook-Pro.local>
2020-12-06 19:18:11 -0500

    add first iteration of proof of work function

--- single_miner_coin/src/user_chain.rs
+++ single_miner_coin/src/user_chain.rs
@@ -13,6 +13,19 @@ struct UserBlock {
 }
 
 impl UserBlock {
+
+    fn calculate_proof_of_work(hash: Hash) -> Hash {
+        let mut proof: u32 = 0;
+
+        loop {
+            let proof_hash: Hash = sha256_hash(format!("{}", proof).as_bytes());
+            let combined_hash = format!("{:?}", sha256_hash(format!("{:?}{:?}", hash, proof_hash).as_bytes())).to_string();
+
+            if &combined_hash[..6] == "000000" {
+                return proof_hash;
+            }
+        }
+    }
     
     pub fn hash(&self) -> Hash {
         sha256_hash(format!("{}{:?}", self.username, self.key).as_bytes())
